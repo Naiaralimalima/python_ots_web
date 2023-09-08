84739e4690c2f4bd34473980d8172f22092c6a5b
Naiaralimalima lima <naicardoso144@gmail.com>
2023-09-08 00:09:32 +0000

    adicionando estilo

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 #importar o flask pip install flask
-#pip freeze> requeriments.txt 
+#importar o requerimrntopip freeze> requeriments.txt 
 
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask("ola mundo")
-
+#lista de postagem mock
+post = [
+    {
+    "titulo":"minha primeira postagem",
+    "texto":"teste"
+    },
+    {
+        "titulo":"segundo post",
+        "texto":"outro teste"
+    }
+    ]
+#rota
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def exibir_entradas():
+    entradas = post #mock das postagens 
+    return render_template('exibir_entradas.html', entradas=entradas)
 
+ 
